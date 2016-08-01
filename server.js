36d99fad14c6c8e4cf9109d43e49b0fa92d030a8
rahmona-henry <rahmona.henry@icloud.com>
2016-08-01 20:45:50 +1200

    Add sign up page - wip.

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ var app = express()
 
 app.use(express.static('client'))
 app.set('view engine','hbs')
+// app.set('views', path.join(_dirname,'client'))
 
 app.get('/', function(req, res){
   res.send('index.html')
+})
+
+app.get('/signUp', function(req, res){
+  res.render('signUp')
 })
 
 app.listen(3000, function(){
