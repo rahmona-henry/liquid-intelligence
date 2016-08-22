a1663e578809f09a2b68c9920793635fcfb2b1cd
rahmona-henry <rahmona.henry@icloud.com>
2016-08-23 11:53:18 +1200

    Created Brews table for new beers:

---
+++ migrations/20160823113022_brews.js
@@ -0,0 +1,34 @@
+
+exports.up = function(knex, Promise) {
+  console.log('create table')
+
+  return knex.schema.createTableIfNotExists('brews', function(table){
+    table.increments('id');
+    table.string('brewName');
+    table.string('brewer');
+    table.string('brewStyle');
+    table.integer('batchNumber');
+    table.string('brewIngredients');
+    table.string('brewingProcess');
+    table.string('brewDate');
+    table.string('bottlingDate');
+    table.string('mashTime');
+    table.string('boilTime');
+    table.integer('original');
+    table.integer('final');
+    table.integer('mashTemperature');
+    table.integer('fermentTemperature');
+    table.integer('batchSize');
+    table.integer('abv');
+    table.string('tastingNotes');
+
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('brews').then(function(){
+    console.log('brews table was droppped')
+
+  })
+
+};
