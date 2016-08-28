06585615759210af92ff2c5764e5e66d15ff3814
rahmona-henry <rahmona.henry@icloud.com>
2016-08-28 16:38:03 +1200

    getAndShow brew function setup

--- src/index.js
+++ src/index.js
@@ -3,4 +3,24 @@ var brewProfile = require('../views/brewProfile.hbs')
 var handlebars = require('handlebars')
 var request = require('superagent')
 var $ = require('jquery')
-  
+
+
+$(document).ready(function(){
+  $('#showBtn').click(function(){
+    showAllBrews()
+  })
+})
+
+function showAllBrews() {
+    request.get('/allbrews')
+    .end(function(err,res){
+     var list = allbrews{{brew:res.body}}
+     document.body.html = listen
+
+     $('viewBtn').click(function(e){
+       e.preventDefault()
+       var id = e.target.id
+       getAndShowProfile(id)
+     })
+    })
+}
