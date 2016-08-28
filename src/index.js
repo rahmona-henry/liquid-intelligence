06613e60efbbc6aa44fab94769868e9f60dc5321
rahmona-henry <rahmona.henry@icloud.com>
2016-08-28 16:25:43 +1200

    Created a index.js file and added JQuery and Superagent to the package.json

---
+++ src/index.js
@@ -0,0 +1,6 @@
+var allBrews = require('..views/allBrews.hbs')
+var brewProfile = require('../views/brewProfile.hbs')
+var handlebars = require('handlebars')
+var request = require('superagent')
+var $ = require('jquery')
+  
