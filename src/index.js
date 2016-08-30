ed3e584ea018f19d4e569252704b89fb7adad498
rahmona-henry <rahmona.henry@icloud.com>
2016-08-30 18:37:07 +1200

    Button to new brews wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 var allBrews = require('..views/allBrews.hbs')
 var brewProfile = require('../views/brewProfile.hbs')
-var handlebars = require('handlebars')
 var request = require('superagent')
 var $ = require('jquery')
 
