3115095b20dd06e7170702eb23d80cb5124170ce
rahmona-henry <rahmona.henry@icloud.com>
2016-08-31 16:53:12 +1200

    WIP - jquery button

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-var allBrews = require('..views/allBrews.hbs')
+var allBrews = require('../views/allbrews.hbs')
 var brewProfile = require('../views/brewProfile.hbs')
+var userHome = require('../views/userHome.hbs')
+var brewOptions = require('../views/brewOptions.hbs')
 var request = require('superagent')
 var $ = require('jquery')
 
 
 $(document).ready(function(){
   $('#showBtn').click(function(){
-    console.log('showBtn is getting clicked')
-    showAllBrews()
+    alert('showBtn is getting clicked')
   })
 })
 
-function showAllBrews() {
-    request.get('/allbrews')
-    .end(function(err,res){
-     var list = allBrews{{data:res.body}}
-     document.body.html = list
-
-     $('viewBtn').click(function(e){
-       e.preventDefault()
-       var id = e.target.id
-       getAndShowProfile(id)
-     })
-    })
-}
+// function showAllBrews() {
+//     request.get('/allbrews')
+//     .end(function(err,res){
+//      var list = allBrews{{data:res.body}}
+//      document.body.html = list
+//
+//      $('.viewBtn').click(function(e){
+//        e.preventDefault()
+//        var id = e.target.id
+//        getAndShowProfile(id)
+//      })
+//     })
+// }
