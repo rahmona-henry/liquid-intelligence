7cad8b58d82afeb794329a7c49ef183ba861bc88
rahmona-henry <rahmona.henry@icloud.com>
2016-10-19 00:36:55 +1300

    Getting places with my jQuery and handlebars

--- src/index.js
+++ src/index.js
@@ -5,11 +5,33 @@ var brewOptions = require('../views/brewOptions.hbs')
 var request = require('superagent')
 var $ = require('jquery')
 
-
 $(document).ready(function(){
+  $('#showBtn').click(function () {
+  showAllBeers()
+  })
 
-  $('#showBtn').click(function(){
-console.log('showBtn is getting clicked')
-})
 
+function showAllBeers() {
+  request.get('/allBrews')
+  .end(function(err, res){
+    var list = allBrews({data:res.body})
+    document.body.innerHTML = list
+    // $('.viewBtn').click(function(e){
+    //   e.preventDefault()
+    //   var id = e.target.id
+    //   getAndShowProfile(id)
+    // })
+  })
+}
+
+// function getAndShowProfile(id) {
+//   request.get('/beers/'+id)
+//   .end(function(err, res){
+//     console.log(res.body)
+//     var htmlFromTemplate = beerProfile(res.body)
+//     document.body.innerHTML = htmlFromTemplate;
+//     $('#backBtn').click(function(){
+//       showAllBeers()
+//     })
+//   })
 })
