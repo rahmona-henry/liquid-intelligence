edd5b1dca2592b2b4aebbf46f1c128618d45988b
rahmona-henry <rahmona.henry@icloud.com>
2016-10-18 23:59:01 +1300

    Finally got my jquery working - oh yagit add -A happy?

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-var allBrews = require('/views/allBrews.hbs')
-var brewProfile = require('/views/brewProfile.hbs')
-var userHome = require('/views/userHome.hbs')
-var brewOptions = require('/views/brewOptions.hbs')
+var allBrews = require('../views/allBrews.hbs')
+var brewProfile = require('../views/brewProfile.hbs')
+var userHome = require('../views/userHome.hbs')
+var brewOptions = require('../views/brewOptions.hbs')
 var request = require('superagent')
 var $ = require('jquery')
 
@@ -9,14 +9,7 @@ var $ = require('jquery')
 $(document).ready(function(){
 
   $('#showBtn').click(function(){
-alert('showBtn is getting clicked')
-    // showAllBrews()
+console.log('showBtn is getting clicked')
 })
 
-// function showAllBrews() {
-//     request.get('/allBrews')
-//     .end(function(err,res){
-//      var list = allBrews{{data:res.body}}
-//      document.body.innerHTML = list
-//     })
-// }
+})
