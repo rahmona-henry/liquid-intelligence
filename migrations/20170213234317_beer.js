98ca204ac21983d28802c68151536351654b729c
rahmona-henry <rahmona.henry@icloud.com>
2017-02-14 00:03:51 +1300

    Add new beer table

---
+++ migrations/20170213234317_beer.js
@@ -0,0 +1,39 @@
+
+exports.up = function(knex, Promise) {
+  console.log('create beertable')
+
+  return knex.schema.createTableIfNotExists('berrtable', function(table){
+    table.increments('id');
+    table.string('emailId');
+    table.string('brewName');
+    table.string('brewStyle');
+    table.string('brewer');
+    table.string('brewDate');
+    table.string('malts');
+    table.string('hops');
+    table.string('yeast');
+    table.string('strikeVolume');
+    table.string('mashTemperature');
+    table.string('mashTime');
+    table.string('spargeWater');
+    table.string('spargeTemperature');
+    table.string('boilVolume');
+    table.string('boilTime');
+    table.integer('original');
+    table.string('fermentTemperature');
+    table.string('fermentTime');
+    table.integer('final');
+    table.string('dextroseDosage');
+    table.string('batchSize');
+    table.integer('abv');
+
+
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('beertable').then(function(){
+    console.log('beertable was dropped')
+  })
+
+};
