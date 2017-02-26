5ad788bd72bb24387f278cad6c9e9c104cc76506
rahmona-henry <rahmona.henry@icloud.com>
2017-02-26 19:38:58 +1300

    show all button not working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var allBrews = require('../views/allBrews.hbs')
-// var brewProfile = require('../views/brewProfile.hbs')
-// var userHome = require('../views/userHome.hbs')
-// var brewOptions = require('../views/brewOptions.hbs')
+var brewProfile = require('../views/brewProfile.hbs')
+var userHome = require('../views/userHome.hbs')
+var brewOptions = require('../views/brewOptions.hbs')
 var request = require('superagent')
 var $ = require('jquery')
 
@@ -14,8 +14,7 @@ $(document).ready(function(){
 
 function showAllBeers() {
   request.get('/allBrews')
-    console.log('This is res',res)
-  .end(function(err,res)
+  .end(function(err,res){
     var list = allBrews({data:res.body})
     document.body.innerHTML = list
 
