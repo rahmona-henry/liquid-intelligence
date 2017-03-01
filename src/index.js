1e46431b5ae7299bb3c689759a0df30ea1508251
rahmona-henry <rahmona.henry@icloud.com>
2017-03-01 23:11:28 +1300

    rendering still a wip

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ var brewProfile = require('../views/brewProfile.hbs')
 var userHome = require('../views/userHome.hbs')
 var brewOptions = require('../views/brewOptions.hbs')
 var request = require('superagent')
+var handlebars = require('handlebars');
 var $ = require('jquery')
 
 $(document).ready(function(){
-
   $('#showBtn').click(function(){
   showAllBeers()
 })
@@ -19,22 +19,22 @@ function showAllBeers() {
     var list = allBrews({data:res.body})
     document.body.innerHTML = list
 
-     $('.viewBtn').click(function(e){
-       e.preventDefault()
-       var id = e.target.id
-       getAndShowProfile(id)
-    })
+    //  $('.viewBtn').click(function(e){
+    //    e.preventDefault()
+    //    var id = e.target.id
+    //    getAndShowProfile(id)
+    // })
   })
 }
 
-function getAndShowProfile(id) {
-   request.get('/brews/'+id)
-   .end(function(err,res){
-     console.log('this is res', res)
-   var htmlFromTemplate = brewProfile(res.body)
-   document.body.innerHTML = htmlFromTemplate
-   $('#backBtn').click(function(){
-   showAllBeers()
-     })
-   })
- }
+// function getAndShowProfile(id) {
+//    request.get('/brews/'+id)
+//    .end(function(err,res){
+//      console.log('this is res', res)
+//    var htmlFromTemplate = brewProfile(res.body)
+//    document.body.innerHTML = htmlFromTemplate
+//    $('#backBtn').click(function(){
+//    showAllBeers()
+//      })
+//    })
+//  }
