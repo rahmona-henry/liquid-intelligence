74f613c127ebb5a9d5fd1469fa81e995cb535367
rahmona-henry <rahmona.henry@icloud.com>
2017-04-17 11:38:58 +1200

    create app.js

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ var path = require('path')
 var session = require('express-session')
 var KnexSessionStore = require('connect-session-knex')(session)
 var store = new KnexSessionStore({knex: knex})
-var Knex = require('knex')
+
 
 app.use(express.static('client'))
 app.set('views',path.join(__dirname,'views'))
 
-// app.use(bodyParser.json())
+
 app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine','hbs')
 app.set('port', (process.env.PORT || 3000));
-app.use(require('session'({
+app.use(session({
   secret: 'liquid network',
   saveUninitialized: true,
   resave: true,
@@ -81,7 +81,7 @@ req.session.destroy()
  ////////// POST ROUTES //////////
 
 app.post('/signUp', function(req,res){
-   var hash = bcrypt.hashSync(req.body.hashed_password, 10)
+   var hash = bcrypt.hashSync(req.body.password, 10)
     knex('users').insert({email:req.body.email, hashed_password:hash})
        .then(function(data){
          console.log('req.body.hashed_password', req.body.hashed_password)
