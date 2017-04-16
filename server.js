e11cddc8d9350b4317688de1f9e3c6786f704675
rahmona-henry <rahmona.henry@icloud.com>
2017-04-17 03:56:28 +1200

    create app.js

--- server.js
+++ server.js
@@ -6,15 +6,17 @@ var path = require('path')
 var session = require('express-session')
 var KnexSessionStore = require('connect-session-knex')(session)
 var store = new KnexSessionStore({knex: knex})
+var Knex = require('knex')
 
 app.use(express.static('client'))
 app.set('views',path.join(__dirname,'views'))
 
-
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine','hbs')
 app.set('port', (process.env.PORT || 3000));
-app.use(session({
+app.use(require('express-session')
+({
   secret: 'liquid network',
   saveUninitialized: true,
   resave: true,
